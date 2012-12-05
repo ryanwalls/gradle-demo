7f8f27c34fd29121c4745f8b5f5322bd54c70362
ryanwalls <github@ryanwalls.com>
2012-12-05 01:47:56 -0700

    Add some more task examples.

--- examples.groovy
+++ examples.groovy
@@ -19,3 +19,57 @@ hello << {
 task wrapper(type: Wrapper) {
     gradleVersion = '1.2'
 }
+
+
+task conversation(dependsOn:hello) << {
+    println "Now we're having a conversation."
+}
+
+task goodbye << {
+    println "Goodbye"
+}
+
+goodbye.dependsOn conversation
+
+task encounter {
+    gradle.taskGraph.whenReady {
+        if (gradle.taskGraph.hasTask(":encounter")) {
+            println "Starting an encounter."
+        }
+    }
+
+    doLast {
+        println "Ending an encounter."
+    }
+}
+
+encounter.dependsOn {
+    tasks.findAll { it.name != 'encounter' }
+}
+
+
+task "taskInside$project.name" << {
+    println "I'm a task for $project.name"
+}
+
+task distribution << {
+    println "We build the zip with version=$version"
+}
+
+task release(dependsOn: 'distribution') << {
+    println 'We release now'
+}
+
+gradle.taskGraph.whenReady {taskGraph ->
+    if (taskGraph.hasTask(release)) {
+        version = '1.0'
+    } else {
+        version = '1.0-SNAPSHOT'
+    }
+}
+
+task copy(type:Copy) {
+    from 'gradle/wrapper'
+    into 'build/testCopy'
+}
+
